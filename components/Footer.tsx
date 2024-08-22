ed15d86df40d4619a977d34264b8d2e0d797611c
Padam7890 <padamthapamoho@gmail.com>
2024-08-22 22:27:22 +0545

    footer addeed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = ({user,type}:FooterProps) => {
   return (
     <footer className=' footer'>
-        <div className="footer_name">
+        <div className={type === 'mobile' ? 'footer_name-mobile': 'footer_name'}>
             <p className=' text-xl font-bold text-gray-700'>
-                {user.firstName}
+                {user?.name[0] || "P"}
             </p>
         </div>
     </footer>
