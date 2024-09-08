555f534682aa6e688fa2d80c096ab35bd4240ed8
Padam7890 <padamthapamoho@gmail.com>
2024-09-08 16:14:50 +0545

    login screen

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 export default function RootLayout({
     children,
@@ -5,9 +6,13 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
     return (
-       <main>
-          SIdebar 
+       <main className=" flex min-h-screen w-full justify-between font-inter ">
           {children}
+          <div className="auth-asset">
+            <div>
+              <Image src={"/icons/auth-image.svg"} alt="auth Image" width={500} height={500}/>
+            </div>
+          </div>
        </main>
     );
   }
